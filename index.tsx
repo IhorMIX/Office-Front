a70f9a94f63e4685a193975cbfb9b552b6ac8b69
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-07-15 05:52:48 +0300

    index and app

---
+++ index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './src/redux/store';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AuthLayout, Layout } from './Components/Layout';
+import MainPage from './pages/MainPage';
+import AuthPage from './pages/AuthPage';
+import "./App.css";
 
-const App: React.FC = () => {
-  return <div>Hello, React + TypeScript!</div>;
-};
+function App() {
+  return (
+    <Routes>
+    <Route path="/" element={<AuthLayout />}>
+        <Route index element={<MainPage />} />
+      </Route>
+      <Route path="/" element={<Layout />}>
+        <Route path='/auth' element={<AuthPage />} />
+      </Route>
+   </Routes>
+  );
+}
 
 export default App;
