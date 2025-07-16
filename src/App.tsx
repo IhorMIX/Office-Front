0dc44984a86fa7128c3c12c99dc50bedc4395a5f
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-07-16 16:44:03 +0300

    app.tsx and authpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { AuthLayout, Layout } from './Components/Layout';
+import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import AuthPage from './pages/AuthPage';
-import "./App.css";
+import { useSelector } from 'react-redux';
+import Layout from './Components/Layout';
+import { RootState } from './redux/store';
 
 function App() {
+  const isAuth = useSelector((state: RootState) => state.auth.isAuth);
+
   return (
     <Routes>
-    <Route path="/" element={<AuthLayout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
       </Route>
-      <Route path="/" element={<Layout />}>
-        <Route path='/auth' element={<AuthPage />} />
-      </Route>
-   </Routes>
+      <Route path="/auth" element={isAuth ? <Navigate to="/" replace /> : <AuthPage />} />
+    </Routes>
   );
 }
-
 export default App;
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,13 +1,96 @@
+import { useNavigate } from "react-router-dom";
+import { useActions } from "../Hooks/StoreHook";
+import { ILoginData } from "../types/User";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useLoginMutation } from "../services/authService";
+import { IAuthInformation } from "../types/AuthInfo";
+import styles from "../scss/authPage.module.scss";
+import { Button, Checkbox,  FormHelperText,  Paper, TextField, Typography } from "@mui/material";
 import React from "react";
 
-const MainPage = () => {
+
+const AuthPage = () => {
+
+  const navigate = useNavigate();
+  const [authorize] = useLoginMutation();
+  const { userLogin } = useActions();
+
+  const { handleSubmit, register, formState: { errors }} = useForm<ILoginData>({
+    defaultValues: {
+      login: "",
+      password: "",
+      isNeedToRemember: false,
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<ILoginData> = async (dataS: ILoginData) => {
+    await authorize(dataS)
+      .unwrap()
+      .then((payload: IAuthInformation) => {
+        userLogin(payload);
+
+        navigate("/");
+      })
+      .catch((error: string) => {
+        console.log(error)
+      });
+  };
+
+  const loginLabel = "Login";
+  const passwordLabel = "Password";
+  const rememberMeLabel = "Remember me";
+  const loginButtonLabel = "Login";
+  const auth = "Auth";
+
   return (
-    <>
-      <div>
-        <h1>AuthPage</h1>
-      </div>     
-    </>
+    <div className={styles.container}>
+      <Paper elevation={4} classes={{ root: styles.root }}>
+        <Typography classes={{ root: styles.title }} variant='h5'>
+          {auth}
+        </Typography>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register("login", {
+              required: "Login is required"
+            })}
+            className={styles.field}
+            label={loginLabel}
+            fullWidth
+            error={!!errors.login}
+            helperText={errors.login && <FormHelperText error>{errors.login.message}</FormHelperText>}
+          />
+
+          <TextField
+            type='password'
+            {...register("password", {
+              required: "Password is required"
+            })}
+            className={styles.field}
+            label={passwordLabel}
+            fullWidth
+            error={!!errors.password}
+            helperText={errors.password && <FormHelperText error>{errors.password.message}</FormHelperText>}
+          />
+
+          <div className={styles.isRemember}>
+            <Checkbox {...register("isNeedToRemember")} />
+            <p>{rememberMeLabel}</p>
+          </div>
+
+          <Button
+            type='submit'
+            size='large'
+            variant='contained'
+            fullWidth
+          >
+            {loginButtonLabel}
+          </Button>
+        </form>
+      </Paper>
+    </div>
   );
 };
 
-export default MainPage;
+export default AuthPage;
