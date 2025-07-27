f6615bd5376940b463fe5be3556cfcee8cfc531e
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-07-27 13:06:44 +0300

    pages

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useGetCurrentUserQuery } from "../services/userService";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { Box, Paper, Typography } from "@mui/material";
 
 const MainPage = () => {
+
+  const {data: user} = useGetCurrentUserQuery(null);
+  const role = useSelector((state: RootState) => state.auth.role);
   return (
-    <>
-      <div>
-        <h1>MainPage</h1>
-      </div>     
-    </>
+   <Box my={4}>
+  <Paper elevation={3} sx={{ padding: 3 }}>
+    <Typography variant="h4" component="h1" gutterBottom>
+      {user?.fullName ?? "Имя не указано"}
+    </Typography>
+    <Typography variant="subtitle1" color="text.secondary">
+      User Type: {role ?? "Неизвестно"}
+    </Typography>
+  </Paper>
+</Box>
   );
 };
 
--- src/pages/ManagersPage.tsx
+++ src/pages/ManagersPage.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useGetAllManagersQuery } from "../services/managerService";
+import { BaseManager } from "../types/Employee";
+import ManagerTable from "../Components/Tables/ManagerTable";
+
 
 const ManagersPage = () => {
+
+  const {data: managersList } = useGetAllManagersQuery(null);
+  const [managers, setManagers] = useState<BaseManager[]>([])
+
+  useEffect(() => {
+    if (managersList) {
+      setManagers(managersList);
+    }
+  }, [managersList]);
+
+
+  const handleEdit = (id: number) => {
+    // Реализация редактирования
+
+  };
+
+  const handleDelete = (id: number) => {
+    // Реализация удаления
+    setManagers(managers.filter((manager) => manager.id !== id));
+  };
+
   return (
     <>
       <div>
         <h1>Managers Page</h1>
       </div>     
+      <ManagerTable managers={managers} onEdit={handleEdit} onDelete={handleDelete}/>
     </>
   );
 };
-
 export default ManagersPage;
