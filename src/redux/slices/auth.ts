6f6227d7b36823de93a6b450620aa8bae0a95a6d
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-07-27 13:06:57 +0300

    styles and other

--- src/redux/slices/auth.ts
+++ src/redux/slices/auth.ts
@@ -1,18 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import { getValue, setValue, removItem } from "../../Helpers/LocalStorageHelper"
+import { getValue, setValue, removeItem } from "../../Helpers/LocalStorageHelper"
 
 import { IsNullOrEmpty } from "../../Helpers/StringHelper";
 import { IReduxAuthModel } from "../../types/Redux";
 import { IAuthInformation } from "../../types/AuthInfo"
-import { AccessKey, RefreshToken } from "../../Helpers/Constants";
+import { AccessKey, RefreshToken, Role } from "../../Helpers/Constants";
 
 const accessKey = getValue(AccessKey);
 const refreshToken = getValue(RefreshToken);
+const role = getValue(Role);
 const initialState: IReduxAuthModel = {
   isAuth: !IsNullOrEmpty(accessKey),
   accessKey: accessKey,
-  refreshToken: refreshToken
+  refreshToken: refreshToken,
+  role: role
 };
 
 const authSlice = createSlice({
@@ -21,15 +23,18 @@ const authSlice = createSlice({
   reducers: {
     userLogout: (state: IReduxAuthModel) => {
       state.isAuth = false;
-      removItem(AccessKey);
-      removItem(RefreshToken)
+      removeItem(AccessKey);
+      removeItem(RefreshToken);
+      removeItem(Role)
     },
     userLogin: (state: IReduxAuthModel, action: { payload: IAuthInformation }) => {
       state.isAuth = !IsNullOrEmpty(action.payload.accessKey);
       state.accessKey = action.payload.accessKey;
       state.refreshToken = action.payload.refresh_token;
+      state.role = action.payload.role;
       setValue(AccessKey, action.payload.accessKey);
       setValue(RefreshToken, action.payload.refresh_token);
+      setValue(Role, action.payload.role);
     }
   }
 });
